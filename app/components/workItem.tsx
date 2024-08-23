cedb7bcd8d81e8ddce46d27e9fcda9c75852372d
Daniel Stadler <danstad2012@gmail.com>
2024-08-22 18:51:17 -0700

    init card reorg

--- app/components/workItem.tsx
+++ app/components/workItem.tsx
@@ -1,43 +1,87 @@
+"use client"
+
 import Link from 'next/link'
 import { formatDate } from 'app/blog/utils'
 import Image from 'next/image'
 import { workObject } from 'app/data/work'
+import { useState } from 'react'
 
 const workList = Object
   .keys(workObject)
   .map(slug => ({ slug, ...workObject[slug] }) )
 
 export function WorkItems() {
+  const [activeIndex, setActiveIndex] = useState(0)
 
   return (
     <div>
-      {workList.map((work) => (
-          <Link
-            key={work.company}
-            className="flex flex-col w-50 space-y-1 mb-4"
-            href={`/work/${work.slug}`}
+      {workList.map((work, idx) => (
+        <div
+          key={work.company}
+          className="flex flex-col w-50 space-y-1 mb-4 border-2 border-blue-200 border-solid p-4 bg-gray-100 rounded-lg"
+        >
+          <div  //header
+            className="flex justify-between items-center bg-gray-200 rounded-lg"
+            onClick={() => setActiveIndex(prev => prev === idx ? -1 : idx)}
           >
-            <div className="flex justify-start items-center bg-gray-100">
-              <Image
-                src={`/image/${work.slug}.jpg`}
-                width={50}
-                height={50}
-                alt={work.company}
-                loading="lazy"
-                decoding="async"
-              />
+            <Image
+              src={`/image/${work.slug}.jpg`}
+              width={100}
+              height={100}
+              alt={work.company}
+              loading="lazy"
+              decoding="async"
+              className='rounded-tl-lg rounded-bl-lg'
+            />
+            <div>
               <p className="text-neutral-600 dark:text-neutral-400 tabular-nums ml-4">
-                {work.company}
+              {work.position} at {work.company}
                 {/* {formatDate(work.metadata.publishedAt, false)} */}
               </p>
-            </div>
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {work.description}
+              <p>
+                {work.date} {work.location}
               </p>
             </div>
-          </Link>
-        ))}
+            <span className='mr-4'> {idx === activeIndex ? '^' : 'v'} </span>
+          </div>
+
+          {activeIndex === idx && <section>
+            <p className="mb-4">
+              {work.tech.map(x => `${x} `)}
+            </p>
+          <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              {work.description}
+            </p>
+          </div>
+
+            <div className="my-8">
+              {work.detail}
+            </div>
+            <div className="my-8">
+              <p className='mb-4'>Gallery</p>
+              {work?.gallery?.map(({ img, alt, desc }) => (
+                <div
+                  key={img}
+                  className="bg-gray-600 mb-4 p-4 rounded-lg"
+                >
+                  <Image
+                    src={`/image/${img}`}
+                    className="rounded-lg"
+                    width={900}
+                    height={50}
+                    alt={alt}
+                    loading="lazy"
+                    decoding="async"
+                    key={img}
+                  />
+                  <p className='mt-4'>{desc}</p>
+                </div>
+              ))}
+            </div>
+          </section>}
+        </div>
+      ))}
     </div>
   )
 }
